5fdcc446d4dc7381c7cf8940b5c54df7be7f3156
Barry <Barry-B15@users.noreply.github.com>
2020-06-10 00:29:44 +0900

    webpack merged dist content getting distroyed

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function(req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('dist/index.html', { root: __dirname })
 })
 
 // designates what port the app will listen to for incoming requests
