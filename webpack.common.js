7f69b76f86dcc2b995a5ebb30c5ac99fa311c437
Barry <Barry-B15@users.noreply.github.com>
2020-06-10 18:06:25 +0900

    fix for merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack')
+
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,40 +2,41 @@ const path = require('path')
 const webpack = require('webpack')
 
 //for merge
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+/* const merge = require('webpack-merge');
+const common = require('./webpack.common.js'); */
 
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(common, {
-    entry: './src/client/index.js',
-    output: { // added from forum
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    mode: 'production',
-    devtool: 'source-map', // for merge
-    module: {
-        rules: [{
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
+module.exports = //merge(common, 
+    {
+        entry: './src/client/index.js',
+        output: { // added from forum
+            libraryTarget: 'var',
+            library: 'Client'
+        },
+        mode: 'production',
+        devtool: 'source-map', // for merge
+        module: {
+            rules: [{
+                    test: '/\.js$/',
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.scss$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/client/views/index.html",
+                filename: "./index.html",
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css"
+            }),
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        }),
-    ]
-});
+    };
